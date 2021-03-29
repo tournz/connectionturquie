2d058062aafe4086c3ccb90d65d8db1dbaf73525
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-03-29 17:12:28 +0300

    Created homepage with sticky navbar and short description

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,11 @@
+const myFunction = () => {
+  const navbar = document.querySelector(".navbar");
+  const sticky = navbar.offsetTop;
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+export { myFunction };
