5f65a5265aef7e93d55160f07f7aad6a11f20dbc
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-03-30 18:56:12 +0300

    added user log in and sign up still need to test

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,27 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+end
