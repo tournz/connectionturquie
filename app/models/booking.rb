f21159234fb5fccaec3da9526bcafb89bdb27bde
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-04-04 00:59:59 +0300

    minor changes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :user
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
+  def end_time_cannot_be_before_start_time
+    if end_time < start_time
+      errors.add(:end_time, "end time cannot be before start time")
+    end
+  end
 end
